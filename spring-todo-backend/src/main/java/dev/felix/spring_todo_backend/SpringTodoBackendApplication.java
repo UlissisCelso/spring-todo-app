a660c50d02171898a36a17b34caf6cac0d6255a3
lucasfelixgit <felixworkspace7@gmail.com>
2025-08-15 18:28:57 -0300

    first commit

---
+++ spring-todo-backend/src/main/java/dev/felix/spring_todo_backend/SpringTodoBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.felix.spring_todo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoBackendApplication.class, args);
+	}
+
+}
---
+++ spring-todo-backend/src/test/java/dev/felix/spring_todo_backend/SpringTodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.felix.spring_todo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
