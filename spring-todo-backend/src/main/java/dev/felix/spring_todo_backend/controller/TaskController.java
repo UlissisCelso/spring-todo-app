a6cf3fa9a3eff5053e65dcdfe37cb0d524666c9c
lucasfelixgit <felixworkspace7@gmail.com>
2025-08-15 20:11:12 -0300

    feat: implement TaskController and TaskService crud functionalities

--- spring-todo-backend/src/main/java/dev/felix/spring_todo_backend/controller/TaskController.java
+++ spring-todo-backend/src/main/java/dev/felix/spring_todo_backend/controller/TaskController.java
@@ -1,17 +1,87 @@
 package dev.felix.spring_todo_backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.felix.spring_todo_backend.entity.Task;
+import dev.felix.spring_todo_backend.service.TaskService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/task")
 public class TaskController {
 
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
     @GetMapping("/greetings")
     public String greetings() {
         return "Hello, this is my springboot app!";
     }
 
-}
+    /*
+    * Implement CRUD functions
+    * -GET
+    * -GET BY ID
+    * -POST
+    * -PUT
+    * -DELETE
+    * */
+
+    @GetMapping("/get")
+    public ResponseEntity<List<Task>> getAll() {
+        List<Task> allTasks = taskService.getAll();
+        return ResponseEntity.ok(allTasks);
+    }
+
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<?> getByID(@PathVariable Long id) {
+        Task optTask = taskService.getByID(id);
+
+        if (optTask != null) {
+            return  ResponseEntity.ok(optTask);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("The task with id: " + id + " was not found.");
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Task> create(@RequestBody Task task) {
+        Task createdTask = taskService.create(task);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(task);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Task task){
+        Task optTask = taskService.getByID(id);
+
+        if (optTask != null) {
+            Task updatedTask = taskService.update(id, task);
+            return ResponseEntity.ok(updatedTask);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("The task with id: " + id + " was not found.");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        Task optTask = taskService.getByID(id);
+
+        if (optTask != null) {
+            taskService.delete(id);
+            return ResponseEntity.ok().body("The task with id: " + id + " was successfully deleted.");
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("The task with id: " + id + " was not found.");
+    }
+
+    }
--- spring-todo-backend/src/main/java/dev/felix/spring_todo_backend/service/TaskService.java
+++ spring-todo-backend/src/main/java/dev/felix/spring_todo_backend/service/TaskService.java
@@ -1,7 +1,42 @@
 package dev.felix.spring_todo_backend.service;
 
+import dev.felix.spring_todo_backend.entity.Task;
+import dev.felix.spring_todo_backend.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAll() {
+        return taskRepository.findAll();
+    }
+
+    public Task getByID(Long id) {
+        Optional<Task> optTask = taskRepository.findById(id);
+        return optTask.orElse(null);
+    }
+
+    public Task create(Task task){
+        return taskRepository.save(task);
+    }
+
+    public Task update(Long id, Task task) {
+        task.setId(id);
+        taskRepository.save(task);
+        return task;
+    }
+
+    public void delete(Long id) {
+        taskRepository.deleteById(id);
+    }
+
 }
